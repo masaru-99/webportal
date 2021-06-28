37366a24c8464c68d2d429f5c8266cc9ff513b39
masaru <masaru@192.168.11.15>
2021-06-28 13:58:53 +0900

    zipcode

--- src/main/java/jp/ac/hcs/s3a126/zipcode/ZipCodeController.java
+++ src/main/java/jp/ac/hcs/s3a126/zipcode/ZipCodeController.java
@@ -1,5 +1,23 @@
 package jp.ac.hcs.s3a126.zipcode;
 
-public class ZipCodeController {
+import java.security.Principal;
 
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+public class ZipCodeController {
+	
+	/**
+	 * 郵便番号から住所を検索し、結果画面を表示する。
+	 * @param zipcode 検索する郵便番号（ハイフンなし）
+	 * @param principal ログイン情報
+	 * @param model
+	 * @return 結果画面 - 郵便番号
+	 */
+	@PostMapping("/zip")
+	public String getZipCode(@RequestParam("zipcode") String zipcode,
+			Principal principal, Model model) {
+		return "zipcode/zipcode";
+	}
 }
