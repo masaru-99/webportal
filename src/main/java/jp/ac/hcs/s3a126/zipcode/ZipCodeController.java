a6c4903d1b3dbcab56a30f8ea1015cbaade2b47f
masaru <masaru@192.168.11.15>
2021-06-28 14:15:38 +0900

    アプリケーションログ実装

--- src/main/java/jp/ac/hcs/s3a126/zipcode/ZipCodeController.java
+++ src/main/java/jp/ac/hcs/s3a126/zipcode/ZipCodeController.java
@@ -2,10 +2,15 @@ package jp.ac.hcs.s3a126.zipcode;
 
 import java.security.Principal;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
 public class ZipCodeController {
 	
 	/**
@@ -18,6 +23,7 @@ public class ZipCodeController {
 	@PostMapping("/zip")
 	public String getZipCode(@RequestParam("zipcode") String zipcode,
 			Principal principal, Model model) {
+		log.info("[" + principal.getName() + "]住所検索：---");
 		return "zipcode/zipcode";
 	}
 }
