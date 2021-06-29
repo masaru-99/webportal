b9dfed252618968cc4cf304fc39a804a861b3bae
masaru <masaru@192.168.11.15>
2021-06-29 11:37:55 +0900

    住所検索機能実装完了

---
+++ src/main/java/jp/ac/hcs/s3a126/WebConfig.java
@@ -0,0 +1,14 @@
+package jp.ac.hcs.s3a126;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class WebConfig {
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+
+}
